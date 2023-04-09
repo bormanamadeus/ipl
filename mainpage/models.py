090ef3b0efe9a9ae965dc4e4bee8e39fedf444d4
roman_pop_os_home_pycharm <rwr79@mail.ru>
2023-04-09 20:10:22 +0400

    finish main page

--- mainpage/models.py
+++ mainpage/models.py
@@ -5,8 +5,10 @@ from django.db import models
 class Services(models.Model):
     title = models.CharField(max_length=50, verbose_name='Услуга')
     content = models.TextField(null=True, blank=True, verbose_name='Описание')
-    price = models.FloatField(null=True, blank=True, verbose_name='Цена')
+    #price = models.FloatField(null=True, blank=True, verbose_name='Цена')
+    price = models.TextField(null=True, blank=True, verbose_name='Расценки')
     document = models.FileField(upload_to='clip', default='', max_length=255, verbose_name='Образец письма')
+    icon = models.FileField(upload_to='icon', default='', max_length=255, verbose_name='Иконка услуги')
     published = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name='Опубликовано')
 
     class Meta:
