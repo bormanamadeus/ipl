cf604dd2acc0842cf3b73073e94de059095b6b55
roman_pop_os_home_pycharm <rwr79@mail.ru>
2023-06-13 03:39:13 +0400

    new main page

--- mainpage/views.py
+++ mainpage/views.py
@@ -13,11 +13,9 @@ def get_main_page(request, **kwargs):
     context = create_context(request)
     return render(request, 'main/main.html', context)
 
-
 def get_services(request, **kwargs):
     context = create_context(request)
     return render(request, 'services/services.html', context)
-
 
 def get_contacts(request, **kwargs):
     context = create_context(request)
@@ -32,5 +30,5 @@ def create_context(request):
     services = Services.objects.all()
     path = request.path[1:-1]
     context = {'services': services, 'path': path}
-    print('Путь: ', request.path)
+    print('Path: ', request.path)
     return context
