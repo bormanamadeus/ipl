faa25c476fe66c44bc1183fb30441ea6f7459ddf
roman_pop_os_home_pycharm <rwr79@mail.ru>
2023-11-28 06:53:07 +0400

    Change menu

--- mainpage/models.py
+++ mainpage/models.py
@@ -7,8 +7,10 @@ class Services(models.Model):
     content = models.TextField(null=True, blank=True, verbose_name='Описание')
     #price = models.FloatField(null=True, blank=True, verbose_name='Цена')
     price = models.TextField(null=True, blank=True, verbose_name='Расценки')
-    document = models.FileField(upload_to='clip', null=True, default='', blank=True, max_length=255, verbose_name='Образец письма')
-    icon = models.FileField(upload_to='icon', null=True, default='', blank=True, max_length=255, verbose_name='Иконка услуги')
+    document = models.FileField(upload_to='clip', null=True, default='', 
+                                blank=True, max_length=255, verbose_name='Образец письма')
+    icon = models.FileField(upload_to='icon', null=True, default='', 
+                            blank=True, max_length=255, verbose_name='Иконка услуги')
     published = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name='Опубликовано')
 
     class Meta:
