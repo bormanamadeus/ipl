a20b90c642a64c5348730dd5b9a4b42d8d42b2bf
roman_pop_os_home_pycharm <rwr79@mail.ru>
2023-12-26 03:20:44 +0400

    Finish spec version for visually impaired peoples

--- static/ipl/src/spec-version.js
+++ static/ipl/src/spec-version.js
@@ -1,32 +1,51 @@
-const buttonDisappear = document.querySelector("#button-for-disappear-box");
-const box = document.querySelector("#box");
+window.addEventListener("load", () => {  
+    console.log("onload Job");
+    const buttonDisappear = document.querySelector("#button-for-disappear-box");
+    const box = document.querySelector("#prev-box-spec");
 
-buttonDisappear.addEventListener("click", ()=>{
-    box.classList.toggle("hidden");
-    buttonDisappear.classList.toggle("hidden");
-})
+    buttonDisappear.addEventListener("click", ()=>{
+        console.log("Job");
+        console.log("box.classList");
+        box.classList.toggle("hidden");
+        buttonDisappear.classList.toggle("hidden");
+    })
 
-const buttonAppear = document.querySelector("#button-for-appear-box");
+    const buttonAppear = document.querySelector("#button-for-appear-box");
 
-buttonAppear.addEventListener("click", ()=>{
-    box.classList.toggle("hidden");
-    buttonDisappear.classList.toggle("hidden");
+    buttonAppear.addEventListener("click", ()=>{
+        box.classList.toggle("hidden");
+        buttonDisappear.classList.toggle("hidden");
 
+        var r = document.querySelector(':root');
+        r.style.setProperty('--main-color', r.style.getPropertyValue('--main-color-def'))
+        r.style.setProperty('--main-size', r.style.getPropertyValue('--main-size-def'))
+        saveVarInLocalStorage("--main-color", r.style.getPropertyValue('--main-color-def'));
+        saveVarInLocalStorage("--main-size", r.style.getPropertyValue('--main-size-def'));
+    })
+
+    console.log("save");
     var r = document.querySelector(':root');
-    r.style.setProperty('--main-color', r.style.getPropertyValue('--main-color-def'))
-    r.style.setProperty('--main-size', r.style.getPropertyValue('--main-size-def'))
-    saveVarInLocalStorage("--main-color", r.style.getPropertyValue('--main-color-def'));
-    saveVarInLocalStorage("--main-size", r.style.getPropertyValue('--main-size-def'));
-})
+    if (r.style.getPropertyValue('--main-size') != r.style.getPropertyValue('--main-size-def') || 
+        (r.style.getPropertyValue('--color-size') != r.style.getPropertyValue('--main-color-def'))) {
+        console.log("in save");
+        box.classList.toggle("hidden");
+        buttonDisappear.classList.toggle("hidden");
+    }
+});
 
 function changeColor(color) {
     var r = document.querySelector(':root');
     if (color === 'A1') {
-        r.style.setProperty('--main-color', 'lightblue')
+        //r.style.setProperty('--main-color', 'lightblue')
+        r.style.setProperty('--main-color', 'white')
     } else if (color === 'A2') {
-        r.style.setProperty('--main-color', 'yellow')
+        r.style.setProperty('--main-color', 'black')
     } else if (color === 'A3') {
-        r.style.setProperty('--main-color', 'green')
+        r.style.setProperty('--main-color', 'blue')
+    } else if (color === 'A4') {
+        r.style.setProperty('--main-color', 'yellow')
+    } else if (color === 'A5') {
+        r.style.setProperty('--main-color', 'brown')
     } else {
         r.style.setProperty('--main-color', 'black')
     }
@@ -36,11 +55,15 @@ function changeColor(color) {
 function changeFontSize(fontsize) {
     var r = document.querySelector(':root');
     if (fontsize === 'S1') {
-        r.style.setProperty('--main-size', '14px')
+        r.style.setProperty('--main-size', '12px')
     } else if (fontsize === 'S2') {
-        r.style.setProperty('--main-size', '20px')
+        r.style.setProperty('--main-size', '14px')
     } else if (fontsize === 'S3') {
-        r.style.setProperty('--main-size', '24px')
+        r.style.setProperty('--main-size', '18px')
+    } else if (fontsize === 'S4') {
+        r.style.setProperty('--main-size', '22px')
+    } else if (fontsize === 'S5') {
+        r.style.setProperty('--main-size', '26px')
     } else {
         r.style.setProperty('--main-size', '14px')
     }
@@ -62,11 +85,5 @@ document.addEventListener("DOMContentLoaded", () => {
     }
     if (mainColor) {
         r.style.setProperty('--main-color', mainColor)
-    }
-
-    if (r.style.getPropertyValue('--main-size') != r.style.getPropertyValue('--main-size-def') || 
-        (r.style.getPropertyValue('--color-size') != r.style.getPropertyValue('--main-color-def'))) {
-        box.classList.toggle("hidden");
-        buttonDisappear.classList.toggle("hidden");
     }
 });
