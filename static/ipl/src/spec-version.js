6a2caf0b23fba5555e878254bf50ad7b2c5f2312
roman_pop_os_home_pycharm <rwr79@mail.ru>
2023-12-26 01:47:41 +0400

    Do save main-color and main-size to go any page of site

---
+++ static/ipl/src/spec-version.js
@@ -0,0 +1,72 @@
+const buttonDisappear = document.querySelector("#button-for-disappear-box");
+const box = document.querySelector("#box");
+
+buttonDisappear.addEventListener("click", ()=>{
+    box.classList.toggle("hidden");
+    buttonDisappear.classList.toggle("hidden");
+})
+
+const buttonAppear = document.querySelector("#button-for-appear-box");
+
+buttonAppear.addEventListener("click", ()=>{
+    box.classList.toggle("hidden");
+    buttonDisappear.classList.toggle("hidden");
+
+    var r = document.querySelector(':root');
+    r.style.setProperty('--main-color', r.style.getPropertyValue('--main-color-def'))
+    r.style.setProperty('--main-size', r.style.getPropertyValue('--main-size-def'))
+    saveVarInLocalStorage("--main-color", r.style.getPropertyValue('--main-color-def'));
+    saveVarInLocalStorage("--main-size", r.style.getPropertyValue('--main-size-def'));
+})
+
+function changeColor(color) {
+    var r = document.querySelector(':root');
+    if (color === 'A1') {
+        r.style.setProperty('--main-color', 'lightblue')
+    } else if (color === 'A2') {
+        r.style.setProperty('--main-color', 'yellow')
+    } else if (color === 'A3') {
+        r.style.setProperty('--main-color', 'green')
+    } else {
+        r.style.setProperty('--main-color', 'black')
+    }
+    saveVarInLocalStorage("--main-color", r.style.getPropertyValue('--main-color'));
+}
+
+function changeFontSize(fontsize) {
+    var r = document.querySelector(':root');
+    if (fontsize === 'S1') {
+        r.style.setProperty('--main-size', '14px')
+    } else if (fontsize === 'S2') {
+        r.style.setProperty('--main-size', '20px')
+    } else if (fontsize === 'S3') {
+        r.style.setProperty('--main-size', '24px')
+    } else {
+        r.style.setProperty('--main-size', '14px')
+    }
+    saveVarInLocalStorage("--main-size", r.style.getPropertyValue('--main-size'));
+} 
+
+function saveVarInLocalStorage(nameVariable, valueVariable) {
+    console.log(nameVariable, valueVariable);
+    localStorage.setItem(nameVariable, valueVariable);
+}
+
+// window.addEventListener('load', () => {
+document.addEventListener("DOMContentLoaded", () => {  
+    var r = document.querySelector(':root');
+    var mainFontSize = localStorage.getItem("--main-size");
+    var mainColor = localStorage.getItem("--main-color");
+    if (mainFontSize) {
+        r.style.setProperty('--main-size', mainFontSize)
+    }
+    if (mainColor) {
+        r.style.setProperty('--main-color', mainColor)
+    }
+
+    if (r.style.getPropertyValue('--main-size') != r.style.getPropertyValue('--main-size-def') || 
+        (r.style.getPropertyValue('--color-size') != r.style.getPropertyValue('--main-color-def'))) {
+        box.classList.toggle("hidden");
+        buttonDisappear.classList.toggle("hidden");
+    }
+});
