07676c79f803d2e2bace7c0c60d4a1484baf8c14
roman_pop_os_home_pycharm <rwr79@mail.ru>
2023-12-26 06:59:52 +0400

    correct color of text main.html

--- static/ipl/src/spec-version.js
+++ static/ipl/src/spec-version.js
@@ -17,16 +17,20 @@ window.addEventListener("load", () => {
         buttonDisappear.classList.toggle("hidden");
 
         var r = document.querySelector(':root');
-        r.style.setProperty('--main-color', r.style.getPropertyValue('--main-color-def'))
-        r.style.setProperty('--main-size', r.style.getPropertyValue('--main-size-def'))
+        r.style.setProperty('--main-color', r.style.getPropertyValue('--main-color-def'));
+        r.style.setProperty('--text-color', r.style.getPropertyValue('--text-color-def'));
+        r.style.setProperty('--extra-back-color', r.style.getPropertyValue('--extra--back-color-def'));
+        r.style.setProperty('--main-size', r.style.getPropertyValue('--main-size-def'));
         saveVarInLocalStorage("--main-color", r.style.getPropertyValue('--main-color-def'));
+        saveVarInLocalStorage("--text-color", r.style.getPropertyValue('--text-color-def'));
+        saveVarInLocalStorage("--extra-back-color", r.style.getPropertyValue('--extra-back-color-def'));
         saveVarInLocalStorage("--main-size", r.style.getPropertyValue('--main-size-def'));
     })
 
     console.log("save");
     var r = document.querySelector(':root');
     if (r.style.getPropertyValue('--main-size') != r.style.getPropertyValue('--main-size-def') || 
-        (r.style.getPropertyValue('--color-size') != r.style.getPropertyValue('--main-color-def'))) {
+        (r.style.getPropertyValue('--main-color') != r.style.getPropertyValue('--main-color-def'))) {
         console.log("in save");
         box.classList.toggle("hidden");
         buttonDisappear.classList.toggle("hidden");
@@ -37,35 +41,49 @@ function changeColor(color) {
     var r = document.querySelector(':root');
     if (color === 'A1') {
         //r.style.setProperty('--main-color', 'lightblue')
-        r.style.setProperty('--main-color', 'white')
+        r.style.setProperty('--main-color', 'white');
+        r.style.setProperty('--extra-back-color', 'white');
+        r.style.setProperty('--text-color', 'black');
     } else if (color === 'A2') {
-        r.style.setProperty('--main-color', 'black')
+        r.style.setProperty('--main-color', 'black');
+        r.style.setProperty('--extra-back-color', 'black');
+        r.style.setProperty('--text-color', 'white');
     } else if (color === 'A3') {
-        r.style.setProperty('--main-color', 'blue')
+        r.style.setProperty('--main-color', 'blue');
+        r.style.setProperty('--extra-back-color', 'blue');
+        r.style.setProperty('--text-color', 'black');
     } else if (color === 'A4') {
-        r.style.setProperty('--main-color', 'yellow')
+        r.style.setProperty('--main-color', 'yellow');
+        r.style.setProperty('--extra-back-color', 'yellow');
+        r.style.setProperty('--text-color', 'black');
     } else if (color === 'A5') {
-        r.style.setProperty('--main-color', 'brown')
+        r.style.setProperty('--main-color', 'brown');
+        r.style.setProperty('--extra-back-color', 'brown');
+        r.style.setProperty('--text-color', 'yellow');
     } else {
-        r.style.setProperty('--main-color', 'black')
+        r.style.setProperty('--main-color', 'black');
+        r.style.setProperty('--extra-back-color', 'black');
+        r.style.setProperty('--text-color', 'white');
     }
     saveVarInLocalStorage("--main-color", r.style.getPropertyValue('--main-color'));
+    saveVarInLocalStorage("--text-color", r.style.getPropertyValue('--text-color'));
+    saveVarInLocalStorage("--extra-back-color", r.style.getPropertyValue('--extra-back-color'));
 }
 
 function changeFontSize(fontsize) {
     var r = document.querySelector(':root');
     if (fontsize === 'S1') {
-        r.style.setProperty('--main-size', '12px')
+        r.style.setProperty('--main-size', '14px')
     } else if (fontsize === 'S2') {
-        r.style.setProperty('--main-size', '14px')
-    } else if (fontsize === 'S3') {
         r.style.setProperty('--main-size', '18px')
-    } else if (fontsize === 'S4') {
+    } else if (fontsize === 'S3') {
         r.style.setProperty('--main-size', '22px')
-    } else if (fontsize === 'S5') {
+    } else if (fontsize === 'S4') {
         r.style.setProperty('--main-size', '26px')
+    } else if (fontsize === 'S5') {
+        r.style.setProperty('--main-size', '30px')
     } else {
-        r.style.setProperty('--main-size', '14px')
+        r.style.setProperty('--main-size', '34px')
     }
     saveVarInLocalStorage("--main-size", r.style.getPropertyValue('--main-size'));
 } 
@@ -80,10 +98,18 @@ document.addEventListener("DOMContentLoaded", () => {
     var r = document.querySelector(':root');
     var mainFontSize = localStorage.getItem("--main-size");
     var mainColor = localStorage.getItem("--main-color");
+    var textColor = localStorage.getItem("--text-color");
+    var backColor = localStorage.getItem("--extra-back-color");
     if (mainFontSize) {
-        r.style.setProperty('--main-size', mainFontSize)
+        r.style.setProperty('--main-size', mainFontSize);
     }
     if (mainColor) {
-        r.style.setProperty('--main-color', mainColor)
+        r.style.setProperty('--main-color', mainColor);
+    }
+    if (textColor) {
+        r.style.setProperty('--text-color', textColor);
+    }
+    if (backColor) {
+        r.style.setProperty('--extra-back-color', backColor);
     }
 });
